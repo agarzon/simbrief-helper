f42f5da53cd0a110346c0b998e4ed4af933e14b4
Alexander Garzon <agarzon@users.noreply.github.com>
2020-02-10 12:20:17 -0500

    Update simbrief_helper.lua
    Added TOC winds, ISA Dev and Temp
    Removed AVG winds, ISA dev and Temp
    Added colors by groups (fuel, TOC and metar)
    Privacy for username

--- Scripts/simbrief_helper.lua
+++ Scripts/simbrief_helper.lua
@@ -7,9 +7,16 @@
 -- TODO: 
 -- * Get RWY magnetic heading
 
--- Variables
+-- Modules
 require("LuaXml")
+-- replace mbox.print() with print()
+--require("mbox")
+--print = mbox.print
 
+-- Useful debug tool
+--local inspect = require 'inspect'
+
+-- Variables
 local socket = require "socket"
 local http = require "socket.http"
 local LIP = require("LIP");
@@ -90,12 +97,14 @@ function readXML()
     DataOfp["MinTakeoff"] = xfile:find("min_takeoff")[1]
     DataOfp["ReserveFuel"] = xfile:find("reserve")[1]
     DataOfp["Payload"] = xfile:find("payload")[1]
+    DataOfp["Pax"] = xfile:find("passengers")[1]
     DataOfp["Zfw"] = xfile:find("est_zfw")[1]
     DataOfp["CostIndex"] = xfile:find("costindex")[1]
-    DataOfp["AvgWindDir"] = xfile:find("avg_wind_dir")[1]
-    DataOfp["AvgWindSpd"] = xfile:find("avg_wind_spd")[1]
-    DataOfp["AvgTempDev"] = xfile:find("avg_temp_dev")[1]
-    DataOfp["Pax"] = xfile:find("passengers")[1]
+    local TOC = xfile:find("navlog"):find(nil,nil,nil,"TOC");
+    DataOfp["CrzWindDir"] = TOC:find("wind_dir")[1]
+    DataOfp["CrzWindSpd"] = TOC:find("wind_spd")[1]
+    DataOfp["CrzTemp"] = TOC:find("oat")[1]
+    DataOfp["CrzTempDev"] = TOC:find("oat_isa_dev")[1]
 
     return true
 end
@@ -105,27 +114,33 @@ function sb_on_build(sb_wnd, x, y)
     readSettings()
 
     imgui.TextUnformatted(string.format("Welcome, %s (%s)", DataOfp["Cpt"], DataOfp["Callsign"]))
+    
+    if imgui.TreeNode("Simbrief") then
+        -- INPUT
+        local changed, userNew = imgui.InputText("Username", sbUser, 255)
 
-    -- INPUT
-    local changed, userNew = imgui.InputText("Simbrief username", sbUser, 255)
-
-    if changed then
-        sbUser = userNew
-        local newSettings =
-        {
-            simbrief =
+        if changed then
+            sbUser = userNew
+            local newSettings =
             {
-                username = userNew,
-            },
-        };
-        
-        saveSettings(newSettings)
+                simbrief =
+                {
+                    username = userNew,
+                },
+            };
+            
+            saveSettings(newSettings)
+        end
+
+        imgui.TreePop()
     end
+
 
     -- BUTTON
     if imgui.Button("Fetch data") then
         if fetchData() then
             readXML()
+            
             clickFetch = true
         end
     end
@@ -157,62 +172,66 @@ function sb_on_build(sb_wnd, x, y)
         imgui.TextUnformatted(string.format("%s (%d ft) - %s (%d ft)", DataOfp["Origin"], DataOfp["Origlevation"], DataOfp["Destination"], DataOfp["DestElevation"]))
 
         imgui.TextUnformatted("                                                  ")
+        imgui.PushStyleColor(imgui.constant.Col.Text, 0xFFFFFF00)
+            imgui.TextUnformatted("Block Fuel:")
+            imgui.SameLine()
+            imgui.TextUnformatted(string.format("%d %s", DataOfp["RampFuel"], DataOfp["Units"]))
 
-        imgui.TextUnformatted("Block Fuel:")
-        imgui.SameLine()
-        imgui.TextUnformatted(string.format("%d %s", DataOfp["RampFuel"], DataOfp["Units"]))
+            imgui.TextUnformatted("Reserve fuel:")
+            imgui.SameLine()
+            imgui.TextUnformatted(string.format("%d %s", DataOfp["ReserveFuel"], DataOfp["Units"]))
 
-        imgui.TextUnformatted("Reserve fuel:")
-        imgui.SameLine()
-        imgui.TextUnformatted(string.format("%d %s", DataOfp["ReserveFuel"], DataOfp["Units"]))
+            imgui.TextUnformatted("Minimum T/O fuel:")
+            imgui.SameLine()
+            imgui.TextUnformatted(string.format("%d %s", DataOfp["MinTakeoff"], DataOfp["Units"]))
+        imgui.PopStyleColor()
 
-        imgui.TextUnformatted("Minimum T/O fuel:")
-        imgui.SameLine()
-        imgui.TextUnformatted(string.format("%d %s", DataOfp["MinTakeoff"], DataOfp["Units"]))
-        
         imgui.TextUnformatted("Payload:")
         imgui.SameLine()
         imgui.TextUnformatted(string.format("%d %s", DataOfp["Payload"], DataOfp["Units"]))
-
-        imgui.TextUnformatted("Pax:")
-        imgui.SameLine()
-        imgui.TextUnformatted(string.format("%d", DataOfp["Pax"]))
-
+        
         imgui.TextUnformatted("ZFW:")
         imgui.SameLine()
         imgui.TextUnformatted(string.format("%d", DataOfp["Zfw"]))
 
-        imgui.TextUnformatted("                                                  ")
-
-        imgui.TextUnformatted("Avg wind:")
+        imgui.TextUnformatted("Pax:")
         imgui.SameLine()
-        imgui.TextUnformatted(string.format("%03d/%03d", DataOfp["AvgWindDir"], DataOfp["AvgWindSpd"]))
-
-        imgui.TextUnformatted("Avg ISA:")
-        imgui.SameLine()
-        imgui.TextUnformatted(string.format("%03d", DataOfp["AvgTempDev"]))
+        imgui.TextUnformatted(string.format("%d", DataOfp["Pax"]))
 
         imgui.TextUnformatted("Cost Index:")
         imgui.SameLine()
         imgui.TextUnformatted(string.format("%d", DataOfp["CostIndex"]))
 
         imgui.TextUnformatted("                                                  ")
+        imgui.PushStyleColor(imgui.constant.Col.Text, 0xFF00FF00)
+            imgui.TextUnformatted("TOC Wind:")
+            imgui.SameLine()
+            imgui.TextUnformatted(string.format("%03d/%03d", DataOfp["CrzWindDir"], DataOfp["CrzWindSpd"]))
 
-        imgui.TextUnformatted(string.format("%s", DataOfp["OrigMetar"]))
+            imgui.TextUnformatted("TOC Temp:")
+            imgui.SameLine()
+            imgui.TextUnformatted(string.format("%03d", DataOfp["CrzTemp"]))
 
-        imgui.TextUnformatted(string.format("%s", DataOfp["DestMetar"]))
+            imgui.TextUnformatted("TOC ISA Dev:")
+            imgui.SameLine()
+            imgui.TextUnformatted(string.format("%03d", DataOfp["CrzTempDev"]))
+        imgui.PopStyleColor()
 
+        imgui.TextUnformatted("                                                  ")
+        imgui.PushStyleColor(imgui.constant.Col.Text, 0xFF00BFFF)
+            imgui.TextUnformatted(string.format("%s", DataOfp["OrigMetar"]))
+            imgui.TextUnformatted(string.format("%s", DataOfp["DestMetar"]))
+        imgui.PopStyleColor()
     end
 
 end
-
 
 -- Open and close window from Lua menu
 
 sb_wnd = nil
 
 function sb_show_wnd()
-    sb_wnd = float_wnd_create(650, 430, 1, true)
+    sb_wnd = float_wnd_create(650, 450, 1, true)
     float_wnd_set_title(sb_wnd, "Simbrief Helper")
     float_wnd_set_imgui_builder(sb_wnd, "sb_on_build")
     float_wnd_set_onclose(sb_wnd, "sb_hide_wnd")
@@ -246,4 +265,3 @@ end
 
 add_macro("Simbrief Helper", "sb_show_wnd()", "sb_hide_wnd()", "deactivate")
 create_command("FlyWithLua/SimbriefHelper/show_toggle", "open/close Simbrief Helper", "toggle_simbrief_helper_interface()", "", "")
-
